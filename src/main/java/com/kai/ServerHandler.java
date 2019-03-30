706c23c7a9ac91486d91f0d4fc73a5b17ab603b3
guoguangkai <guoguangkai@example.com>
2019-03-30 09:21:55 +0800

    添加测试

---
+++ src/main/java/com/kai/ServerHandler.java
@@ -0,0 +1,27 @@
+package com.kai;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		//((ByteBuf)msg).release();
+		try {
+			
+		}finally {
+			ReferenceCountUtil.release(msg);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	
+
+}
